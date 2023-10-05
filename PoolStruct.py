ebf602a34dd91f1936a65a3515a7d796372c5680
Masterain <i@irain.in>
2023-10-04 21:19:30 -0700

    Update data

--- PoolStruct.py
+++ PoolStruct.py
@@ -5,11 +5,13 @@ from PoolLocalizationStruct import PoolLocalizationStruct
 class PoolStruct(dict):
     def __init__(self, version: str, start_time: datetime, end_time: datetime, uigf_type: int, order: int,
                  up_orange1_item: int, up_purple1_item: int, up_purple2_item: int, up_purple3_item: int,
-                 up_orange2_item: int = 0, up_purple4_item: int = 0, up_purple5_item: int = 0):
+                 up_orange2_item: int = 0, up_purple4_item: int = 0, up_purple5_item: int = 0,
+                 timezone: int = 8):
         super().__init__()
         self["version"] = version
         self["start_time"] = start_time
         self["end_time"] = end_time
+        self["timezone"] = timezone
         self["uigf_type"] = uigf_type
         self["order"] = order
         self["up_orange1_item"] = up_orange1_item
